65514bbefc3471805e93c193e5ed43ca4daba3ed
Stefan <stef.kir1999@gmail.com>
2026-03-15 01:07:54 +0400

    Оптимизирован Main лишним кодом и перегрузками

--- src/main/java/com/superchack/Main.java
+++ src/main/java/com/superchack/Main.java
@@ -3,14 +3,20 @@ package com.superchack;
 import com.superchack.validation.IPValidator;
 import com.superchack.validation.PortValidator;
 import com.superchack.validation.Space;
+import com.superchack.validation.ComputerOSValidator;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Super Chack Messenger v1.0");
         System.out.println("Режим: p2p(без сервера)");
-        Space.put();
+
+        Space.print();
         PortValidator.findFreePort();
-        Space.put();
+
+        Space.print();
         System.out.println(IPValidator.getNetworkInfo());
+
+        Space.print();
+        ComputerOSValidator.print();
     }
 }
